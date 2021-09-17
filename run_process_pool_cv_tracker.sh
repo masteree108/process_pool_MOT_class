197412630cd6d69e9f9d58e8a5b23c893ce6bda1
ivan_yu <gun123kk@gmail.com>
2021-09-18 02:24:30 +0800

    modified multi-process architecture,speed last than previous code, but test it on the win10 is ok

--- run_process_pool_cv_tracker.sh
+++ run_process_pool_cv_tracker.sh
@@ -1,1 +1,2 @@
-python main.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video video/race_clip_1s_1.mp4
+#python main.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video video/race_clip_1s_1.mp4
+python main.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --video video/15_clip_1s_1.mp4
